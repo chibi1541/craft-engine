1e871a812bf591f26c1f2a71aa95b63802e01e7c
BOOK-P5J347BQL9\user <runic1541@gmail.com>
2026-08-28 17:37:18 +0900

    MultyThread 환경 구축 #3(작업 완료)
    ThreadManager 추가
    
    이후 추가 작업이 필요한 경우 지속

--- Source/Thread/ThreadManager.cpp
+++ Source/Thread/ThreadManager.cpp
@@ -1,2 +1,57 @@
 ﻿#include "pch.h"
+#include "TLS.h"
 #include "ThreadManager.h"
+
+NAME_SPACE_BEGIN(Craft)
+
+ThreadManager::ThreadManager()
+{
+	// 메인 쓰레드의 TLS 초기화
+	InitTLS();
+}
+
+ThreadManager::~ThreadManager()
+{
+	Join();
+	DestroyTLS();
+}
+
+void ThreadManager::Launch(function<void()> work)
+{
+	std::lock_guard<std::mutex> guard(_lock);
+
+	_threads.emplace_back(
+		// 쓰레드 생성(쓰레드가 실행할 로직)
+		std::thread([=]()
+			{
+				InitTLS();
+				work();
+				DestroyTLS();
+			})
+	);
+}
+
+void ThreadManager::Join()
+{
+	for (std::thread& t : _threads)
+	{
+		if (t.joinable())
+		{
+			t.join();
+		}
+	}
+
+	_threads.clear();
+}
+
+void ThreadManager::InitTLS()
+{
+	static atomic<uint32> SThreadId = 1;
+	LThreadId = SThreadId.fetch_add(1);
+}
+
+void ThreadManager::DestroyTLS()
+{
+}
+
+NAME_SPACE_END
--- Source/Thread/ThreadManager.h
+++ Source/Thread/ThreadManager.h
@@ -1,1 +1,34 @@
 ﻿#pragma once
+#include <thread>
+#include <functional>
+#include <mutex>
+
+// ########################
+// #	  ThreadManager	  #
+// ########################
+
+NAME_SPACE_BEGIN(Craft)
+class CRAFT_API ThreadManager
+{
+public:
+	ThreadManager();
+	~ThreadManager();
+
+	// 실행 쓰레드를 생성
+	void Launch(function<void()> work);
+	// 쓰레드가 전부 종료될 때까지(join 상태가 될 때 까지) 대기
+	void Join();
+
+	// Thread-Local-Storage 초기화
+	static void InitTLS();
+	// Thread-Local-Storage 정리 
+	static void DestroyTLS();
+
+
+private:
+	// read-write-lock이 의미가 없어서 mutex를 사용
+	std::mutex _lock;
+	std::vector<std::thread> _threads;
+};
+
+NAME_SPACE_END
